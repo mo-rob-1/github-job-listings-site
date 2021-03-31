b2ef5eb7664feb113f9719fd9da04a85c7c89941
Mohammed Rob <mohammed_rob@hotmail.co.uk>
2021-03-31 16:41:52 +0100

    component update

--- src/Job.js
+++ src/Job.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Card, Badge, Button, Collapse } from 'react-bootstrap';
+import ReactMarkdown from 'react-markdown';
 
 const Job = ({ job }) => {
+    const [open, setOpen] = useState(false);
+
     return (
-        <div>
-            {job.title}
-        </div>
+        <Card className="mb-4">
+            <Card.Body>
+                <div className="d-flex justify-content-between">
+                    <div>
+                        <Card.Title>
+                            {job.title} - <span className="text-muted">{job.company}</span>
+                        </Card.Title>
+                        <Card.Subtitle className="text-muted mb-2">
+                            {new Date(job.created_at).toLocaleDateString()}
+                        </Card.Subtitle>
+                        <Badge variant="secondary" className="mr-2">{job.type}</Badge>
+                        <Badge variant="secondary">{job.location}</Badge>
+                        <div className="mt-3" style={{ wordBreak: 'break-all' }}>
+                            <ReactMarkdown source={job.how_to_apply} />
+                        </div>
+                    </div>
+                <img className="d-none d-md-block" height="50" src={job.company_logo} alt={job.company} />
+                </div>
+                <Card.Text>
+                    <Button 
+                        onClick={() => setOpen(prevOpen => !prevOpen)} 
+                        variant="primary"
+                    >
+                    {open ? 'Hide Details' : 'View Details'}
+                    </Button>
+                </Card.Text>
+                <Collapse in={open}>
+                    <div className="mt-4">
+                        <ReactMarkdown source={job.description} />
+                    </div>
+                </Collapse>
+            </Card.Body>
+        </Card>
     )
 }
 
