d35a55755577108b8a9425cc03c19aeebebf65d6
Mo Rob <31892724+morob1@users.noreply.github.com>
2021-03-31 12:33:13 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useFetchJobs from './useFetchJobs';
+import { Container, Spinner } from 'react-bootstrap';
+import Job from './Job';
 
 function App() {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error } = useFetchJobs(params, page)
+
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <Container>
+      {loading && <Spinner animation="border" variant="dark" />}
+      {error && <h1>Error, Try Refreshing.</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
+    </Container>
   );
 }
 
